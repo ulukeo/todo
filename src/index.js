6f75de39986d22627a5f5d20d257f809431e6b89
ulukeo <ulukimanbaev5@gmail.com>
2022-03-26 17:44:47 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const App = () => {
-  return(
-      <h1>Hi</h1>
+
+const TodoTitle = () => {
+  return (
+    <div>
+      <h1>Todo applications</h1>
+    </div>
   )
 }
 
+
+const TodoSearch = () => {
+  return (
+    <div>
+      <input type='text' placeholder='search' />
+    </div>
+  )
+}
+
+
+const TodoCounter = () => {
+  return (
+    <div>
+      <p>To do: 2</p>
+      <p>Done: 3</p>
+    </div>
+  )
+}
+
+
+const TodoListItem = ({ label }) => {
+  return <span>{label}</span>
+}
+
+
+const TodoList = ({ todos }) => {
+  const elements = todos.map((todo) => {
+    return <li key={todo.id}><TodoListItem label={todo.label} /></li>
+  })
+
+  return (
+    <div>
+      <ul>
+        { elements }
+      </ul>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const todos = [
+    {id: 1, label: 'Drink vodka', done: false, important: false},
+    {id: 2, label: 'Drink tequila', done: false, important: false},
+    {id: 3, label: 'Drink beer', done: false, important: false},
+    {id: 4, label: 'Sleep well', done: false, important: false},
+  ]
+
+  return (
+    <div>
+      <TodoTitle />
+      <TodoSearch />
+      <TodoCounter />
+      <TodoList todos={todos} />
+    </div>
+  )
+}
+
+
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
-);
+)
